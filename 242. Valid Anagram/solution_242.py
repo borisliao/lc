769a4bc165171a038ebf52bc2b59216e9a694bc5
Boris Liao <borisliaos@gmail.com>
2023-08-07 22:31:28 -0400

    Added solution in lc

--- 242. Valid Anagram/solution_242.py
+++ 242. Valid Anagram/solution_242.py
@@ -6,3 +6,25 @@ def isAnagram(s: str, t: str) -> bool:
             return False
         
     return True
+
+def naive(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    dictS = {}
+
+    for char in s:
+        if char in dictS:
+            dictS[char] +=1
+        else:
+            dictS[char] = 1
+    
+    for char in t:
+        if char in dictS:
+            if dictS[char] == 1:
+                del dictS[char]
+            else:
+                dictS[char] -=1
+    
+    return False if len(dictS) > 0 else True
+
