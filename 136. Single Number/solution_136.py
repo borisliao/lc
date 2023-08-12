9030c87cef621e15e2af526e05e1a9b6ed6ba8b2
Boris Liao <borisliaos@gmail.com>
2023-08-12 18:22:33 -0400

    Added solution and notes

--- 136. Single Number/solution_136.py
+++ 136. Single Number/solution_136.py
@@ -16,13 +16,49 @@ def invalid_solution(nums: List[int]) -> int:
     return values[0]
 
 
-def singleNumber(nums: List[int]) -> int:
+def ans_initial_val(nums: List[int]) -> int:
     """
+    Set the ans to the initial value nums[0]
+    but that's not required due to property of xor
+    value b xor with 0 will equal b
+
+    a | b | a ^ b
+    --|---|------
+    0 | 0 | 0 <- this property
+    0 | 1 | 1 <- this property
+    1 | 0 | 1
+    1 | 1 | 0
+
     Time Complexity: O(n)
     Space Complexity: O(1) (constant extra space)
     """
     ans = nums[0]
     for i in range(1, len(nums)):
         ans ^= nums[i]
+
+    return ans
+
+
+def singleNumber(nums: List[int]) -> int:
+    """
+    Set the ans to the initial value 0
+    due to property of xor
+    value b xor with 0 will equal b
+
+    a | b | a ^ b
+    --|---|------
+    0 | 0 | 0 <- this property
+    0 | 1 | 1 <- this property
+    1 | 0 | 1
+    1 | 1 | 0
+
+    the XOR operation is associative and commutative. That means a ^ (b ^ c) = (a ^ b) ^ c, and a ^ b = b ^ a.
+
+    Time Complexity: O(n)
+    Space Complexity: O(1) (constant extra space)
+    """
+    ans = 0
+    for num in nums:
+        ans ^= num
 
     return ans
