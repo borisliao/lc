2a4f90fc81ec74e6f15aa416f218d76dee6482cf
Boris Liao <borisliaos@gmail.com>
2023-08-12 18:13:45 -0400

    Added valid solution

--- 136. Single Number/solution_136.py
+++ 136. Single Number/solution_136.py
@@ -2,11 +2,27 @@ from typing import List
 
 
 def invalid_solution(nums: List[int]) -> int:
-    """This does not satisfy time and memory complexity."""
+    """
+    This solution does not satisfy time and memory complexity.
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+    """
     values = []
-    for i in nums:
-        if i not in values:
+    for i in nums:  # O(n)
+        if i not in values:  # in operator for list is O(n)
             values.append(i)
         else:
             values.remove(i)
     return values[0]
+
+
+def singleNumber(nums: List[int]) -> int:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(1) (constant extra space)
+    """
+    ans = nums[0]
+    for i in range(1, len(nums)):
+        ans ^= nums[i]
+
+    return ans
