fbb8117f9a1a99ac566ca8f0a62f56368ebf3644
Boris Liao <borisliaos@gmail.com>
2023-08-12 18:16:21 -0400

    Commented out incomplete solution

--- 659. Encode and Decode Strings/solution_659.py
+++ 659. Encode and Decode Strings/solution_659.py
@@ -1,21 +1,21 @@
-def encode_decode(func_name):
-    """
-    @param: strs: a list of strings
-    @return: encodes a list of strings to a single string.
-    """
-    def encode(strs):
-        # write your code here
-        pass
+# def encode_decode(func_name):
+#     """
+#     @param: strs: a list of strings
+#     @return: encodes a list of strings to a single string.
+#     """
+#     def encode(strs):
+#         # write your code here
+#         pass
 
-    """
-    @param: str: A string
-    @return: decodes a single string to a list of strings
-    """
-    def decode(str):
-        # write your code here
-        pass
+#     """
+#     @param: str: A string
+#     @return: decodes a single string to a list of strings
+#     """
+#     def decode(str):
+#         # write your code here
+#         pass
 
-    if func_name == 'encode':
-        return encode
-    if func_name == 'decode':
-        return decode
+#     if func_name == 'encode':
+#         return encode
+#     if func_name == 'decode':
+#         return decode
