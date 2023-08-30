163f607b2b7d9b21037b4c1731bb8bb15e0ba593
Boris Liao <borisliaos@gmail.com>
2023-08-30 17:30:54 -0400

    Started 206. Reverse Linked List

---
+++ Linked List/206. Reverse Linked List/solution_206.py
@@ -0,0 +1,26 @@
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        list = []
+
+        def addToList(ln: ListNode, list: List):
+            list.append(ln.val)
+            if (ln.next):
+                addToList(ln.next, list)
+
+        addToList(self, list)
+
+        return str(list)
+
+    def __eq__(self, __value: object) -> bool:
+        return str(self) == str(__value)
+
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    pass
---
+++ Linked List/206. Reverse Linked List/test_solution_206.py
@@ -0,0 +1,35 @@
+import pytest
+import inspect
+import solution_206
+
+
+def ln(list):
+    """From a list, builds a ListNode data structure"""
+    if (list):
+        return solution_206.ListNode(list[0], ln(list[1:]))
+    else:
+        return None
+
+
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_206, predicate=inspect.isfunction)])
+def test_example_1(f):
+    head = ln([1, 2, 3, 4, 5])
+    output = ln([5, 4, 3, 2, 1])
+
+    assert str(f(head)) == str(output)
+
+
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_206, predicate=inspect.isfunction)])
+def test_example_2(f):
+    head = ln([1, 2])
+    output = ln([2, 1])
+
+    assert str(f(head)) == str(output)
+
+
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_206, predicate=inspect.isfunction)])
+def test_example_3(f):
+    head = ln([])
+    output = ln([])
+
+    assert str(f(head)) == str(output)
