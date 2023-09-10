98ba0b334861836abe4a719bde9a88e1cb9dd5ac
Boris Liao <borisliaos@gmail.com>
2023-09-10 17:10:08 -0400

    Added attempt for 19

--- Linked List/19. Remove Nth Node From End of List/solution_19.py
+++ Linked List/19. Remove Nth Node From End of List/solution_19.py
@@ -25,5 +25,29 @@ class ListNode:
         return str(self) == str(__value)
 
 
-def attempt1(head: Optional[ListNode], n: int) -> Optional[ListNode]:
-    pass
+# def two_pass(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#     """
+#     attempt1, Naive approach, 2 passes
+#     fails test case 2 due to flawed nth_from_beginning
+#     """
+#     original_head = head
+#     total = 0
+
+#     while head:
+#         total += 1
+#         head = head.next
+
+#     nth_from_beginning = total - n
+
+#     head = original_head
+
+#     # traverse to nth-1 element from beginning
+#     for _ in range(nth_from_beginning - 1):
+#         head = head.next
+
+#     # -----remove the nth element-----
+#     if head.next:
+#         head.next = head.next.next
+#     else:
+#         head.next = None
+#     return original_head
