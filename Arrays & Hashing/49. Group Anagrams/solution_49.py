5eea7a7f31c2a97a1ed1b56279d50308ebbe5068
Boris Liao <borisliaos@gmail.com>
2023-10-18 23:17:03 -0400

    Reviewed 49. Group Anagrams

--- Arrays & Hashing/49. Group Anagrams/solution_49.py
+++ Arrays & Hashing/49. Group Anagrams/solution_49.py
@@ -9,3 +9,49 @@ def groupAnagrams(strs):
         anagram_map[sorted_word].append(word)
 
     return list(anagram_map.values())
+
+
+def review1(strs):
+    """
+    https://www.youtube.com/watch?v=vzdNOK2oB2E
+    Anki review 10/17/23
+    """
+    anagram_sets = defaultdict(list)
+
+    for word in strs:
+        count = [0] * 26
+        for letter in word:
+            count[ord(letter) - ord("a")] += 1
+        anagram_sets[tuple(count)].append(word)
+
+    return anagram_sets.values()
+
+
+def review2(strs):
+    """
+    Anki review 10/18/23
+    """
+    groups = {}
+    for word in strs:
+        count = [0] * 26
+        for char in word:
+            count[ord(char) - ord('a')] += 1
+        if tuple(count) in groups:
+            groups[tuple(count)].append(count)
+        else:
+            groups[tuple(count)] = [word]
+    return groups.values()
+
+
+def review3(strs):
+    """
+    Anki review 10/18/23
+    same as first solution above
+    """
+    groups = {}
+    for word in strs:
+        if ''.join(sorted(word)) in groups:
+            groups[''.join(sorted(word))].append(word)
+        else:
+            groups[''.join(sorted(word))] = [word]
+    return groups.values()
--- Arrays & Hashing/49. Group Anagrams/test_solution_49.py
+++ Arrays & Hashing/49. Group Anagrams/test_solution_49.py
@@ -6,12 +6,31 @@ import unittest
 case = unittest.TestCase()
 
 # does not work due to nested list not being in the right order
-# @pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
-# def test_example_1(f):
-#     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-#     output = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
 
-#     case.assertCountEqual(f(strs), output)
+
+def SameStuff(l1, l2):
+    if not l1 and not l2:
+        return True
+
+    if len(l1) != len(l2):
+        return False
+
+    last_element = l1[-1]
+    if last_element not in l2:
+        return False
+
+    l1.remove(last_element)
+    l2.remove(last_element)
+
+    return SameStuff(l1, l2)
+
+
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
+def test_example_1(f):
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    output = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+
+    assert SameStuff(f(strs), output)
 
 
 @ pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
