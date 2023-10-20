705a43cb2a1701eb6adb7095c9c9875f1a986cc5
Boris Liao <borisliaos@gmail.com>
2023-10-19 21:13:36 -0400

    Disabled non functional test

--- Arrays & Hashing/49. Group Anagrams/test_solution_49.py
+++ Arrays & Hashing/49. Group Anagrams/test_solution_49.py
@@ -5,32 +5,31 @@ import solution_49
 import unittest
 case = unittest.TestCase()
 
-# does not work due to nested list not being in the right order
+
+# def SameStuff(l1, l2):
+#     if not l1 and not l2:
+#         return True
+
+#     if len(l1) != len(l2):
+#         return False
+
+#     last_element = l1[-1]
+#     if last_element not in l2:
+#         return False
+
+#     l1.remove(last_element)
+#     l2.remove(last_element)
+
+#     return SameStuff(l1, l2)
 
 
-def SameStuff(l1, l2):
-    if not l1 and not l2:
-        return True
+# # does not work due to nested list not being in the right order
+# @pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
+# def test_example_1(f):
+#     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+#     output = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
 
-    if len(l1) != len(l2):
-        return False
-
-    last_element = l1[-1]
-    if last_element not in l2:
-        return False
-
-    l1.remove(last_element)
-    l2.remove(last_element)
-
-    return SameStuff(l1, l2)
-
-
-@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
-def test_example_1(f):
-    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-    output = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
-
-    assert SameStuff(f(strs), output)
+#     assert SameStuff(f(strs), output)
 
 
 @ pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
@@ -42,7 +41,7 @@ def test_example_2(f):
 
 
 @ pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_49, predicate=inspect.isfunction)])
-def test_example_2(f):
+def test_example_3(f):
     strs = ["a"]
     output = [["a"]]
 
