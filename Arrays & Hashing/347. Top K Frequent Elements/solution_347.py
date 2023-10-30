9300b0c491c76c4a001f65c676b8cee1ad7bda61
Boris Liao <borisliaos@gmail.com>
2023-10-29 23:45:54 -0400

    Reviewed 347. Top K Frequent Elements

--- Arrays & Hashing/347. Top K Frequent Elements/solution_347.py
+++ Arrays & Hashing/347. Top K Frequent Elements/solution_347.py
@@ -14,3 +14,23 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
         k_val.append(key)
         k-=1
     return k_val
+
+def review1(nums: List[int], k: int) -> List[int]:
+    """Anki Reviewed 10/29/23"""
+    count = defaultdict(lambda: 0)
+    buckets = [[] for _ in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] += 1
+    
+    for n, frequency in count.items():
+        buckets[frequency].append(n)
+    
+    index = -1
+    result = []
+    while len(result) < k:
+        if buckets[index]:
+            result.append(buckets[index].pop())
+        else:
+            index -= 1
+    return result
