24d2e69893ae54c9b71482ef24b74ed443d28cc4
Boris Liao <borisliaos@gmail.com>
2023-11-09 04:23:52 +0000

    Added 230. Kth Smallest Element in a BST

---
+++ Trees/230. Kth Smallest Element in a BST/solution_230.py
@@ -0,0 +1,59 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        """
+        Source: https://stackoverflow.com/questions/37495634/python-list-from-python-binary-tree
+        """
+        items = []
+        queue = [self]
+
+        while queue:
+            copy = queue[:]
+            queue = []
+
+            for item in copy:
+                if item is None:
+                    items.append(None)
+                    queue.append(None)
+                    queue.append(None)
+                else:
+                    items.append(item.val)
+                    queue.append(item.left)
+                    queue.append(item.right)
+
+            if all((x is None for x in queue)):
+                break
+
+        return str(items)
+
+    def __repr__(self):
+        return f"{self}"
+
+    def __eq__(self, __value: object) -> bool:
+        return str(self) == str(__value)
+
+    def __hash__(self):
+        return id(self)
+
+
+def kthSmallest(root: Optional[TreeNode], k: int) -> int:
+    stack = []
+    c = root
+
+    while stack or c:
+        while c:
+            stack.append(c)
+            c = c.left
+    
+        c = stack.pop()
+        k -= 1
+        if k == 0:
+            return c.val
+        c = c.right
---
+++ Trees/230. Kth Smallest Element in a BST/test_solution_230.py
@@ -0,0 +1,44 @@
+import pytest
+import inspect
+import solution_230
+
+
+def tn(items: list[int]) -> solution_230.TreeNode:
+    """
+    From a list of values, create a TreeNode
+    Source: https://stackoverflow.com/questions/43097045/best-way-to-construct-a-binary-tree-from-a-list-in-python
+    """
+    n = len(items)
+    if n == 0:
+        return None
+
+    def inner(index: int = 0) -> solution_230.TreeNode:
+        """Closure function using recursion bo build tree"""
+        if n <= index or items[index] is None:
+            return None
+
+        node = solution_230.TreeNode(items[index])
+        node.left = inner(2 * index + 1)
+        node.right = inner(2 * index + 2)
+        return node
+
+    return inner()
+
+
+@pytest.mark.timeout(3)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_230, predicate=inspect.isfunction)])
+def test_example_1(f):
+    root = tn([3, 1, 4, None, 2])
+    k = 1
+    output = 1
+
+    assert f(root, k) == output
+
+
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_230, predicate=inspect.isfunction)])
+def test_example_2(f):
+    root = tn([5, 3, 6, 2, 4, None, None, 1])
+    k = 3
+    output = False
+
+    assert f(root, k) == output
