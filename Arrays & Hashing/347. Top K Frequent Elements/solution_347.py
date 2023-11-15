273f648c24c3f7c65081ef0120746a07e1a8cdaa
borisliao <borisliaos@gmail.com>
2023-11-14 20:44:28 -0500

    Reviewed 347. Top K Frequent Elements

--- Arrays & Hashing/347. Top K Frequent Elements/solution_347.py
+++ Arrays & Hashing/347. Top K Frequent Elements/solution_347.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 from collections import defaultdict
 
@@ -5,15 +6,16 @@ from collections import defaultdict
 def topKFrequent(nums: List[int], k: int) -> List[int]:
     mapList = defaultdict(lambda: 0)
     for num in nums:
-        mapList[num] +=1
-    
+        mapList[num] += 1
+
     k_val = []
     for key, val in sorted(mapList.items(), key=lambda item: item[1], reverse=True):
         if k == 0:
             break
         k_val.append(key)
-        k-=1
+        k -= 1
     return k_val
+
 
 def review1(nums: List[int], k: int) -> List[int]:
     """Anki Reviewed 10/29/23"""
@@ -22,10 +24,10 @@ def review1(nums: List[int], k: int) -> List[int]:
 
     for n in nums:
         count[n] += 1
-    
+
     for n, frequency in count.items():
         buckets[frequency].append(n)
-    
+
     index = -1
     result = []
     while len(result) < k:
@@ -34,3 +36,22 @@ def review1(nums: List[int], k: int) -> List[int]:
         else:
             index -= 1
     return result
+
+
+def review2(nums: List[int], k: int) -> List[int]:
+    """
+    Anki 11-14-23
+    Used: [heap docs](https://docs.python.org/3/library/heapq.html), [dict docs](https://realpython.com/iterate-through-dictionary-python/#looping-over-dictionary-items-the-items-method)
+          debugger (3)
+    Time: 28 min
+    """
+    freq = defaultdict(lambda: 0)
+
+    for n in nums:
+        freq[n] += 1
+
+    values = []
+    for n, f in freq.items():
+        heapq.heappush(values, (f, n))
+
+    return [n for f, n in heapq.nlargest(k, values, lambda x: x[0])]
