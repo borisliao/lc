1d98e9c6015b2d8303016824153858f8140a3a83
borisliao <borisliaos@gmail.com>
2023-11-16 23:36:40 -0500

    Reviewed 659. Encode and Decode Strings

--- Arrays & Hashing/659. Encode and Decode Strings/solution_659.py
+++ Arrays & Hashing/659. Encode and Decode Strings/solution_659.py
@@ -31,38 +31,108 @@ def literal_eval(func_name):
         return decode
 
 
-def numberDesc(func_name):
-    def encode(strs: List[str]) -> str:
-        numString = ''
-        for s in strs:
-            numString += str(len(s)) + '#' + s
+# def numberDesc(func_name):
+#     def encode(strs: List[str]) -> str:
+#         numString = ''
+#         for s in strs:
+#             numString += str(len(s)) + '#' + s
 
-        return numString
+#         return numString
+
+#     def decode(str: str) -> List[str]:
+#         stringNum = []
+
+#         word = ''
+#         ignoreDelimitter = False
+#         amountOfChar = 0
+
+#         for i in range(len(str)):
+#             if ignoreDelimitter:
+#                 ignoreDelimitter = False
+#             elif amountOfChar > 0:
+#                 word += str[i]
+#                 amountOfChar -= 1
+#                 if amountOfChar == 0:
+#                     stringNum.append(word)
+#                     word = ''
+#             # might break because we are looking i+1
+#             elif str[i].isnumeric() and str[i+1] == '#':
+#                 amountOfChar = int(str[i])
+#                 ignoreDelimitter = True
+#             else:
+#                 raise ValueError('str is malformed')
+
+#         return stringNum
+
+#     if func_name == 'encode':
+#         return encode
+#     if func_name == 'decode':
+#         return decode
+
+
+# def review1(func_name):
+#     """
+#     Anki 11-16-23
+#     Used: debugger (4)
+#     """
+#     def encode(strs: List[str]) -> str:
+#         result = ''
+#         for s in strs:
+#             result += str(len(s)) + '#' + s  # debugger (1,4)
+
+#         return result  # debugger (2)
+
+#     def decode(str: str) -> List[str]:
+#         i = 0
+#         result = []
+#         while i < len(str)-1:  # debugger (3)
+#             amount = 0
+#             for s in str[i:]:
+#                 if s == "#":  # debugger (5)
+#                     break
+#                 amount += 1
+#             amount = int(str[i:amount])  # debugger (6)
+
+#             begining_of_word = i+amount  # debugger(7)
+#             end_of_word = i+2+amount
+#             result.append(str[begining_of_word:end_of_word])
+#             i = end_of_word
+
+#         return result  # debugger (2)
+
+#     if func_name == 'encode':
+#         return encode
+#     if func_name == 'decode':
+#         return decode
+
+
+def review1(func_name):
+    """
+    Anki 11-16-23
+    Used: debugger (4), lookup solution (1)
+    Time: 9 min
+    """
+    def encode(strs: List[str]) -> str:
+        result = ''
+        for s in strs:
+            result += str(len(s)) + '#' + s  # debugger (1, 3)
+
+        return result
 
     def decode(str: str) -> List[str]:
-        stringNum = []
+        i = 0
+        result = []
+        while i < len(str):  # debugger (2), lookup solution (1)
+            start = i
+            while str[i] != '#':
+                i += 1
+            characters = int(str[start:i])  # debugger (4)
+            beginning = i+1
+            end = beginning + characters
+            result.append(str[beginning:end])
+            i = end
 
-        word = ''
-        ignoreDelimitter = False
-        amountOfChar = 0
-
-        for i in range(len(str)):
-            if ignoreDelimitter:
-                ignoreDelimitter = False
-            elif amountOfChar > 0:
-                word += str[i]
-                amountOfChar -= 1
-                if amountOfChar == 0:
-                    stringNum.append(word)
-                    word = ''
-            # might break because we are looking i+1
-            elif str[i].isnumeric() and str[i+1] == '#':
-                amountOfChar = int(str[i])
-                ignoreDelimitter = True
-            else:
-                raise ValueError('str is malformed')
-
-        return stringNum
+        return result
 
     if func_name == 'encode':
         return encode
