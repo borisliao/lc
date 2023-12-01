defc41e67374901f614fe545992404543efc9f52
borisliao <borisliaos@gmail.com>
2023-11-30 23:26:58 -0500

    Added 78. Subsets

--- Backtracking/78. Subsets/solution_78.py
+++ Backtracking/78. Subsets/solution_78.py
@@ -1,5 +1,27 @@
 from typing import List
 
 
-def subsets(nums: List[int]) -> List[List[int]]:
-    pass
+def neetcode(nums: List[int]) -> List[List[int]]:
+    """ https://www.youtube.com/watch?v=REOH22Xwdkk """
+    res = []
+    subset = []
+
+    def dfs(i):
+        if i >= len(nums):
+            res.append(subset.copy())
+            return
+        # decision to include nums[i]
+        subset.append(nums[i])
+        dfs(i + 1)
+        # decision NOT to include nums[i]
+        subset.pop()
+        dfs(i + 1)
+
+    dfs(0)
+    return res
+
+
+# def review1(nums: List[int]) -> List[List[int]]:
+#     """
+#     """
+#     pass
