ad2425837b967df2290ee089e5f893baf5031d5c
borisliao <borisliaos@gmail.com>
2023-12-03 13:06:47 -0500

    Reviewed 78. Subsets

--- Backtracking/78. Subsets/solution_78.py
+++ Backtracking/78. Subsets/solution_78.py
@@ -21,7 +21,26 @@ def neetcode(nums: List[int]) -> List[List[int]]:
     return res
 
 
-# def review1(nums: List[int]) -> List[List[int]]:
-#     """
-#     """
-#     pass
+def review1(nums: List[int]) -> List[List[int]]:
+    """
+    Anki 1-3-23
+    Used: Peak at solution (2)
+    Time: 13:50
+    [The Backtracking Blueprint: The Legendary 3 Keys To Backtracking Algorithms](https://www.youtube.com/watch?v=Zq4upTEaQyM)
+    """
+    result = []
+    subset = []
+
+    def dfs(i):
+        if i >= len(nums):
+            result.append(subset.copy())  # s2, need to copy
+            return  # s1, did not return
+
+        subset.append(nums[i])
+        dfs(i + 1)
+
+        subset.pop()
+        dfs(i + 1)
+
+    dfs(0)
+    return result
