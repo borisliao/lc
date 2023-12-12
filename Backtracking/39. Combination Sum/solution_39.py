882f76525f6a9c4bcb03a82df85df86945a3aa67
borisliao <borisliaos@gmail.com>
2023-12-12 00:20:43 -0500

    Reviewed 39. Combination Sum

--- Backtracking/39. Combination Sum/solution_39.py
+++ Backtracking/39. Combination Sum/solution_39.py
@@ -138,3 +138,27 @@ def review4(candidates: List[int], target: int) -> List[List[int]]:
 
     dfs(0)  # d1, d3
     return result
+
+
+def review5(candidates: List[int], target: int) -> List[List[int]]:
+    """
+    Anki 12-11-23
+    Time: 12:12
+    """
+    result = []
+    subset = []
+
+    def dfs(i):
+        if sum(subset) == target:
+            result.append(subset.copy())
+            return
+        if i >= len(candidates) or sum(subset) > target:
+            return
+
+        subset.append(candidates[i])
+        dfs(i)
+        subset.pop()
+        dfs(i+1)
+
+    dfs(0)
+    return result
