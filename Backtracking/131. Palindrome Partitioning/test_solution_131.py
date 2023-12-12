49c2a53303a4bf8037e2590bc3f4d9bebe82a375
borisliao <borisliaos@gmail.com>
2023-12-12 01:34:01 -0500

    Started 131. Palindrome Partitioning

---
+++ Backtracking/131. Palindrome Partitioning/test_solution_131.py
@@ -0,0 +1,27 @@
+import pytest
+import inspect
+import solution_131
+
+
+def same_elements(x, y):
+    sorted_x = sorted([sorted(e) for e in x])
+    sorted_y = sorted([sorted(e) for e in y])
+    assert sorted_x == sorted_y
+    return sorted_x == sorted_y
+
+
+@pytest.mark.timeout(3)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_131, predicate=inspect.isfunction)])
+def test_example_1(f):
+    s = "aab"
+    output = [["a", "a", "b"], ["aa", "b"]]
+
+    assert same_elements(f(s), output)
+
+
+@pytest.mark.timeout(3)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_131, predicate=inspect.isfunction)])
+def test_example_2(f):
+    s = "a"
+    output = [["a"]]
+    assert same_elements(f(s), output)
