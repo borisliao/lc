eb0f4558378d5969212f90d09be3c7d3b110eaea
borisliao <borisliaos@gmail.com>
2023-12-15 17:06:18 -0500

    Reviewed 81. Search in Rotated Sorted Array II

--- Miscellaneous/81. Search in Rotated Sorted Array II/solution_81.py
+++ Miscellaneous/81. Search in Rotated Sorted Array II/solution_81.py
@@ -30,6 +30,43 @@ def neetCode(nums: List[int], target: int) -> bool:
                 r = m - 1
 
         else:
-            l+=1
+            l += 1
+
+    return False
+
+
+def review1(nums: List[int], target: int) -> bool:
+    """
+    Anki 12-15-23
+    Time: 23:06
+    """
+    l = 0
+    r = len(nums)-1
+
+    while l <= r:
+        m = (l+r)//2
+
+        if nums[m] == target:
+            return True
+
+        side = 'unknown'  # s1
+
+        if nums[m] > nums[l]:  # s2, nums[-1]
+            side = 'left'
+        elif nums[m] < nums[l]:  # s2, nums[-1]
+            side = 'right'
+
+        if side == 'left':
+            if nums[0] <= target < nums[m]:
+                r = m-1
+            else:
+                l = m + 1
+        elif side == 'right':
+            if nums[m] < target <= nums[-1]:
+                l = m + 1
+            else:
+                r = m - 1
+        elif side == 'unknown':  # s1
+            l += 1
 
     return False
