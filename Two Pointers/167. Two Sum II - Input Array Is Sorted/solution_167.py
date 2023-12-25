8d7668308d1159c0b3ffd2f657bf64d44234cae6
borisliao <borisliaos@gmail.com>
2023-12-24 22:05:01 -0500

    Reviewed 167. Two Sum II - Input Array Is Sorted

--- Two Pointers/167. Two Sum II - Input Array Is Sorted/solution_167.py
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted/solution_167.py
@@ -23,8 +23,40 @@ def twoPointers(numbers: List[int], target: int) -> List[int]:
 
     while left < right:
         if numbers[left] + numbers[right] > target:
-            right -=1
+            right -= 1
         elif numbers[left] + numbers[right] < target:
-            left +=1
+            left += 1
         else:
-            return [left+1,right+1]
+            return [left+1, right+1]
+
+
+def review1(numbers: list[int], target: int) -> list[int]:
+    """
+    Anki 12-24-23
+    Time: 20 min
+    O(n) space complexity
+    """
+    count = {}
+
+    for i, n in enumerate(numbers):
+        if target - n in count:
+            return [count[target-n]+1, i+1]
+        count[n] = i
+
+
+def review2(numbers: list[int], target: int) -> list[int]:
+    """
+    Anki 12-24-23
+    Time: 10 min
+    Used: [Two Sum II - Amazon Coding Interview Question - Leetcode 167 - Python](https://www.youtube.com/watch?v=cQ1Oz4ckceM), debugger 1, solution 1
+    """
+    l = 0
+    r = len(numbers) - 1
+
+    while l < r:  # s1 <=
+        if numbers[l] + numbers[r] == target:
+            return [l+1, r+1]
+        elif numbers[l] + numbers[r] > target:
+            r -= 1
+        else:
+            l += 1  # d2 l-=1
