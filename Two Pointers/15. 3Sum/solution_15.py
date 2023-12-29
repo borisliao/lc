ae29af6c5772fd9a978c01d5964b22d346d506be
borisliao <borisliaos@gmail.com>
2023-12-28 23:58:45 -0500

    Reviewed 15. 3Sum

--- Two Pointers/15. 3Sum/solution_15.py
+++ Two Pointers/15. 3Sum/solution_15.py
@@ -71,7 +71,7 @@ def review2(nums: list[int]) -> list[list[int]]:
     nums.sort()
     result = []
 
-    for i in len(range(nums)):
+    for i in range(len(nums)):
         l = i+1
         r = len(nums) - 1
 
@@ -90,4 +90,37 @@ def review2(nums: list[int]) -> list[list[int]]:
                 r -= 1
                 while nums[l] == nums[l - 1] and l < r:  # s1
                     l += 1  # s1
+    return result
+
+
+def review3(nums: list[int]) -> list[list[int]]:
+    """
+    Anki 12-28-23
+    Time: 14 min
+    Used: debugger 3, solution 1
+    """
+    nums.sort()
+    result = []
+
+    for i in range(len(nums)):
+        l = i + 1  # d3 +1
+        r = len(nums) - 1
+
+        # d1 access r before decoration, d3 used nums[r] instead of i, s3 i <= r
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        while l < r:
+            total = nums[i] + nums[l] + nums[r]
+            if total == 0:
+                result.append([nums[i], nums[l], nums[r]])  # d2 added []
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+            elif total > 0:
+                r -= 1
+            else:
+                l += 1
+
     return result
