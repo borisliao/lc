3566d9bf513c2dab833fd13a5a5aa6fd98158e1a
borisliao <borisliaos@gmail.com>
2023-12-31 12:27:48 -0500

    Reviewed 70. Climbing Stairs

--- 1-D Dynamic Programming/70. Climbing Stairs/solution_70.py
+++ 1-D Dynamic Programming/70. Climbing Stairs/solution_70.py
@@ -43,9 +43,9 @@ def climbStairs(n: int) -> int:
     return dfs(n)
 
 
-def climbStairs(n: int) -> int:
+def review1(n: int) -> int:
     """
-    Anki 12-22-23
+    Anki 12-29-23
     Time: 30 min
     Used: debugger
     """
@@ -72,3 +72,31 @@ def climbStairs(n: int) -> int:
             return i1 + i2  # d1 add memoization
 
     return dfs(0)
+
+
+# def review3(n: int) -> int:
+#     """
+#     Anki 12-29-23
+#     Used: solution, [Amazon Coding Interview Question - Recursive Staircase Problem](https://www.youtube.com/watch?v=5o-kdjv7FD0)
+#     Brute force solution, same as climbStairs()
+#     """
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return review3(n-1) + review3(n-2)
+
+
+def review4(n: int) -> int:
+    """
+    Anki 12-31-23
+    Time: 20 min
+    Used: [Climbing Stairs - Dynamic Programming - Leetcode 70 - Python](https://www.youtube.com/watch?v=Y0lT9Fck7qI)
+    """
+    one, two = 1, 1
+
+    for _ in range(n - 1):
+        temp = one
+        one = one + two
+        two = temp
+
+    return one
