1e0dccef7115516900abb23d14ee6762c6c99433
borisliao <borisliaos@gmail.com>
2023-12-31 16:43:19 -0500

    Reviewed 125. Valid Palindrome

--- Two Pointers/125. Valid Palindrome/solution_125.py
+++ Two Pointers/125. Valid Palindrome/solution_125.py
@@ -41,3 +41,28 @@ def review1(s: str) -> bool:
         l += 1
         r -= 1
     return True
+
+
+def review2(s: str) -> bool:
+    """
+    Anki 12-31-23
+    Time: 7 min
+    Used: debugger 1
+    """
+    l = 0
+    r = len(s) - 1
+
+    while l <= r:
+        if s[l] == ' ' or not s[l].isalnum():
+            l += 1
+            continue
+        if s[r] == ' ' or not s[r].isalnum():
+            r -= 1
+            continue
+
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1  # d1
+        r -= 1  # d1
+
+    return True
