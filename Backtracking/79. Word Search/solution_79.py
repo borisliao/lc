d5bd8513e91a7e5e2616903dcfefe288fcbe5ba4
borisliao <borisliaos@gmail.com>
2023-12-31 13:35:28 -0500

    Reviewed 79. Word Search

--- Backtracking/79. Word Search/solution_79.py
+++ Backtracking/79. Word Search/solution_79.py
@@ -212,3 +212,28 @@ def review4(board: list[list[str]], word: str) -> bool:
                 return True
 
     return False
+
+
+def review5(board: list[list[str]], word: str) -> bool:
+    """
+    Anki 12-31-23
+    Time: 10 min
+    """
+    visited = set()
+
+    def dfs(r, c, i):
+        if i >= len(word):
+            return True
+
+        if r < 0 or c < 0 or r >= len(board) or c >= len(board[r]) or board[r][c] != word[i] or (r, c) in visited:
+            return False
+
+        visited.add((r, c))
+        return dfs(r-1, c, i+1) or dfs(r+1, c, i+1) or dfs(r, c-1, i+1) or dfs(r, c+1, i+1)
+
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            if dfs(r, c, 0):
+                return True
+
+    return False
