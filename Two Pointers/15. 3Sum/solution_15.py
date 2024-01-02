75d34a9c0740465223768700a4c17a60093d16ff
borisliao <borisliaos@gmail.com>
2024-01-02 18:57:52 -0500

    Reviewed 15. 3Sum

--- Two Pointers/15. 3Sum/solution_15.py
+++ Two Pointers/15. 3Sum/solution_15.py
@@ -216,3 +216,33 @@ def review6(nums: list[int]) -> list[list[int]]:
                     l += 1
 
     return result  # d1
+
+
+def review7(nums: list[int]) -> list[list[int]]:
+    """
+    Anki 1-2-24
+    Time: 7 min
+    Used: solution 1
+    """
+    nums.sort()
+    result = []
+
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:  # s1 while
+            continue
+
+        l = i+1
+        r = len(nums) - 1
+        while l < r:
+            total = nums[i] + nums[l] + nums[r]
+            if total > 0:
+                r -= 1
+            elif total < 0:
+                l += 1
+            else:
+                result.append([nums[i], nums[l], nums[r]])
+                l += 1
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+
+    return result
