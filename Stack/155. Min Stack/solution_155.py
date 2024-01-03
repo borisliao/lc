8b136eca0c9e44752fba85b245e0abb3c395a508
borisliao <borisliaos@gmail.com>
2024-01-03 18:36:33 -0500

    Added 155. Min Stack

--- Stack/155. Min Stack/solution_155.py
+++ Stack/155. Min Stack/solution_155.py
@@ -1,22 +1,34 @@
-# def minStack():
-#     class MinStack:
+def minStack():
+    """
+    1-3-24
+    Time: 30 min
+    Used: Debugger 2
+    """
+    class MinStack:
 
-#         def __init__(self):
-#             pass
+        def __init__(self):
+            self.min = float('inf')  # d1 float('-inf')
+            self.lookup = {}
+            self.stack = []
 
-#         def push(self, val: int) -> None:
-#             pass
+        def push(self, val: int) -> None:
+            self.stack.append(val)
+            self.lookup[(val, len(self.stack))] = self.min
+            self.min = min(self.min, val)
 
-#         def pop(self) -> None:
-#             pass
+        def pop(self) -> None:
+            val = self.stack.pop()  # d2 val, length
+            # d2 (val, length)
+            self.min = self.lookup[(val, len(self.stack) + 1)]
+            del self.lookup[(val, len(self.stack) + 1)]  # d2 (val, length)
 
-#         def top(self) -> int:
-#             pass
+        def top(self) -> int:
+            return self.stack[-1]
 
-#         def getMin(self) -> int:
-#             pass
+        def getMin(self) -> int:
+            return self.min
 
-#     return MinStack()
+    return MinStack()
 
 
 # def minStack():
