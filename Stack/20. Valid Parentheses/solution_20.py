a845ee5af87d2e4f50c9fd5e5cc3edd4093ade7d
borisliao <borisliaos@gmail.com>
2024-01-03 17:53:47 -0500

    Added 20. Valid Parentheses

--- Stack/20. Valid Parentheses/solution_20.py
+++ Stack/20. Valid Parentheses/solution_20.py
@@ -1,2 +1,26 @@
-# def isValid(s: str) -> bool:
+def isValid(s: str) -> bool:
+    stack = []
+    for c in s:
+        if c == '(' or c == '{' or c == '[':
+            stack.append(c)
+            continue
+
+        stack_val = stack.pop() if stack else None
+
+        if (
+            stack_val == '(' and c == ')' or
+            stack_val == '{' and c == '}' or
+            stack_val == '[' and c == ']'
+        ):  # d2 flipped strings
+            continue
+        else:
+            return False
+
+    return True  # d1
+
+
+# def review1(s: str) -> bool:
+#     """
+#     Anki
+#     """
 #     pass
