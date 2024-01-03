39c695748bd3cc32300b45ee50f74804aa4dfec4
borisliao <borisliaos@gmail.com>
2024-01-03 17:41:40 -0500

    Started 20. Valid Parentheses

---
+++ Stack/20. Valid Parentheses/test_solution_20.py
@@ -0,0 +1,30 @@
+import pytest
+import inspect
+import solution_20
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_20, predicate=inspect.isfunction)])
+def test_example_1(f):
+    s = "()"
+    output = True
+
+    assert f(s) == output
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_20, predicate=inspect.isfunction)])
+def test_example_2(f):
+    s = "()[]{}"
+    output = True
+
+    assert f(s) == output
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_20, predicate=inspect.isfunction)])
+def test_example_3(f):
+    s = "(]"
+    output = False
+
+    assert f(s) == output
