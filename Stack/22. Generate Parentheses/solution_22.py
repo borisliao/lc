3d1f7d10ab4678df140d69023aa9aea0afdb520c
borisliao <borisliaos@gmail.com>
2024-01-07 11:46:56 -0500

    Reviewed 22. Generate Parentheses

--- Stack/22. Generate Parentheses/solution_22.py
+++ Stack/22. Generate Parentheses/solution_22.py
@@ -27,5 +27,23 @@ def generateParenthesis(n: int) -> list[str]:
 def review1(n: int) -> list[str]:
     """
     Anki 1-7-24
+    Time: 17 min
     """
-    pass
+    stack = []
+    result = []
+
+    def dfs(open, close):
+        if open == close == n:
+            result.append("".join(stack))
+            return
+        if open < n:
+            stack.append('(')
+            dfs(open+1, close)
+            stack.pop()
+        if open > close:
+            stack.append(')')
+            dfs(open, close + 1)
+            stack.pop()
+
+    dfs(0, 0)
+    return result
