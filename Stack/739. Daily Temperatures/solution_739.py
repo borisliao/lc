be8459cbe503319a3a1a950f4558569082e4fadb
borisliao <borisliaos@gmail.com>
2024-01-07 16:54:32 -0500

    Added 739. Daily Temperatures

--- Stack/739. Daily Temperatures/solution_739.py
+++ Stack/739. Daily Temperatures/solution_739.py
@@ -1,2 +1,14 @@
 def dailyTemperatures(temperatures: list[int]) -> list[int]:
-    pass
+    """
+    Anki 1-7-24
+    Used: [Daily Temperatures - Monotonic Stack - Leetcode 739 - Python](https://www.youtube.com/watch?v=cTBiBSnjO3c)
+    """
+    result = [0] * len(temperatures)
+    stack = []
+
+    for i, t in enumerate(temperatures):
+        while stack and t > temperatures[stack[-1]]:
+            index = stack.pop()
+            result[index] = (i - index)
+        stack.append(i)
+    return result
