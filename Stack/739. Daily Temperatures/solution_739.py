06210f741c63846de0a9eebbf353d579971fc819
borisliao <borisliaos@gmail.com>
2024-01-07 21:16:30 -0500

    Reviewed 739. Daily Temperatures

--- Stack/739. Daily Temperatures/solution_739.py
+++ Stack/739. Daily Temperatures/solution_739.py
@@ -46,3 +46,23 @@ def review2(temperatures: list[int]) -> list[int]:
         stack.append(i)
 
     return result
+
+
+def review3(temperatures: list[int]) -> list[int]:
+    """
+    1-7-24
+    https://leetcode.com/problems/daily-temperatures/solutions/397728/easy-python-o-n-time-o-1-extra-space-beat-99-9
+    """
+    max_temp = 0
+    result = [0] * len(temperatures)
+    i = len(temperatures) - 1
+    while i >= 0:
+        if temperatures[i] >= max_temp:
+            max_temp = temperatures[i]
+        else:
+            days = 1
+            while temperatures[i] >= temperatures[i+days]:
+                days += result[i+days]
+            result[i] = days
+        i -= 1
+    return result
--- Stack/739. Daily Temperatures/test_solution_739.py
+++ Stack/739. Daily Temperatures/test_solution_739.py
@@ -28,3 +28,21 @@ def test_example_3(f):
     output = [1, 1, 0]
 
     assert f(temperatures) == output
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_739, predicate=inspect.isfunction)])
+def test_lc_5(f):
+    temperatures = [34, 80, 80, 34, 34, 80, 80, 80, 80, 34]
+    output = [1, 0, 0, 2, 1, 0, 0, 0, 0, 0]
+
+    assert f(temperatures) == output
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_739, predicate=inspect.isfunction)])
+def test_lc_6(f):
+    temperatures = [89, 62, 70, 58, 47, 47, 46, 76, 100, 70]
+    output = [8, 1, 5, 4, 3, 2, 1, 1, 0, 0]
+
+    assert f(temperatures) == output
