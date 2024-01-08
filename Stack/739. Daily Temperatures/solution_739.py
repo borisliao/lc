a4672420e24fb9cc149117da9be0b96a74dafc81
borisliao <borisliaos@gmail.com>
2024-01-07 20:32:54 -0500

    Reviewed 739. Daily Temperatures

--- Stack/739. Daily Temperatures/solution_739.py
+++ Stack/739. Daily Temperatures/solution_739.py
@@ -12,3 +12,37 @@ def dailyTemperatures(temperatures: list[int]) -> list[int]:
             result[index] = (i - index)
         stack.append(i)
     return result
+
+
+def review1(temperatures: list[int]) -> list[int]:
+    """
+    Anki 1-7-24
+    """
+    stack = []
+    result = [0] * len(temperatures)
+
+    for i, t in enumerate(temperatures):
+        # s1 missing t >
+        while stack and t > temperatures[stack[-1]]:
+            index = stack.pop()  # s1
+            result[index] = i - index
+        stack.append(i)
+
+    return result
+
+
+def review2(temperatures: list[int]) -> list[int]:
+    """
+    Anki 1-7-24
+    Time: 2 min
+    """
+    stack = []
+    result = [0] * len(temperatures)
+
+    for i, t in enumerate(temperatures):
+        while stack and t > temperatures[stack[-1]]:
+            index = stack.pop()
+            result[index] = i - index
+        stack.append(i)
+
+    return result
