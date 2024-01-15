269d31ba6b80390d725f5f9c2b64d589398f0adf
borisliao <borisliaos@gmail.com>
2024-01-15 15:16:34 -0500

    Reviewed 11. Container With Most Water

--- Two Pointers/11. Container With Most Water/solution_11.py
+++ Two Pointers/11. Container With Most Water/solution_11.py
@@ -1,2 +1,30 @@
 # def maxArea(height: list[int]) -> int:
-#     pass
+#     """
+#     Anki 1-15-24
+#     Used: [Container with Most Water - Leetcode 11 - Python](https://www.youtube.com/watch?v=UuiTKBwPgAo)
+#     Brute Force Solution
+#     """
+#     largest = 0
+#     for l in range(len(height)):
+#         for r in range(l + 1, len(height)):  # s1 l + 1
+#             # d1 parentheses r-l
+#             largest = max(largest, min(height[l], height[r]) * (r- l))
+
+#     return largest
+
+def maxArea(height: list[int]) -> int:
+    """
+    Anki 1-15-24
+    Used: [Container with Most Water - Leetcode 11 - Python](https://www.youtube.com/watch?v=UuiTKBwPgAo)
+    """
+    largest = 0
+    l = 0
+    r = len(height) - 1
+    while l < r:
+        largest = max(largest, min(height[l], height[r]) * (r - l))
+        if height[l] <= height[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return largest
