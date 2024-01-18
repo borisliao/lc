96b5aa794bb90b0b5e275463c01dc910dc8cea80
borisliao <borisliaos@gmail.com>
2024-01-17 21:27:53 -0500

    Reviewed 78. Subsets

--- Backtracking/78. Subsets/solution_78.py
+++ Backtracking/78. Subsets/solution_78.py
@@ -122,3 +122,31 @@ def review4(nums: List[int]) -> List[List[int]]:
 
     dfs(0)
     return result
+
+
+def review5(nums: List[int]) -> List[List[int]]:
+    """
+    Anki 1-17-24
+    Time: 7 min
+    """
+    result = []
+    subset = []
+
+    def dfs(i):
+        if i >= len(nums):
+            result.append(subset.copy())
+            return
+        subset.append(nums[i])
+        dfs(i+1)
+        subset.pop()
+        dfs(i+1)
+
+    dfs(0)
+    return result
+
+
+# def review6(nums: List[int]) -> List[List[int]]:
+#     """
+#     Anki 12-18-23
+#     Time: 7 min
+#     """
