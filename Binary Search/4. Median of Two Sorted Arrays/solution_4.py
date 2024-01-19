a8da35d994f6824ecb67d99104ad0a8be180c098
borisliao <borisliaos@gmail.com>
2024-01-19 18:41:15 -0500

    Reviewed 4. Median of Two Sorted Arrays

--- Binary Search/4. Median of Two Sorted Arrays/solution_4.py
+++ Binary Search/4. Median of Two Sorted Arrays/solution_4.py
@@ -134,12 +134,44 @@ def review3(nums1: list[int], nums2: list[int]) -> float:
         return (merged[middle] + merged[middle+1]) / 2  # d3 parentheses
 
 
-# def review4(nums1: list[int], nums2: list[int]) -> float:
+def review4(nums1: list[int], nums2: list[int]) -> float:
+    """
+    Anki 1-19-24
+    Used: [Median of Two Sorted Arrays - Binary Search - Leetcode 4](https://www.youtube.com/watch?v=q6IEA26hvXc)
+    O(log(n+m))
+    """
+    A, B = nums1, nums2
+    if len(A) > len(B):  # s5 <
+        A, B = B, A
+
+    total = len(nums1) + len(nums2)
+    half = total // 2
+
+    l = 0
+    r = len(A) - 1  # s6 len(A)
+    while True:
+        i = (l+r) // 2
+        j = half-i-2
+
+        a_left = A[i] if i >= 0 else float('-inf')  # s7 i < 0
+        a_right = A[i+1] if i+1 < len(A) else float('inf')
+        b_left = B[j] if j >= 0 else float('-inf')  # s7 i < 0
+        b_right = B[j+1] if j+1 < len(B) else float('inf')  # s8  j+1 < 0
+
+        if a_left <= b_right and b_left <= a_right:  # s4 <
+            if total % 2:
+                return min(a_right, b_right)  # s3 return min(a_left, b_left)
+            # s2 min(a_left, b_left)
+            return (max(a_left, b_left) + min(a_right, b_right)) / 2
+        elif a_left > b_right:
+            r = i-1  # s1 l += 1
+        else:
+            l = j+1  # s1 r -= 1
+
+
+# def review5(nums1: list[int], nums2: list[int]) -> float:
 #     """
-#     Anki 1-18-24
+#     Anki 1-19-24
 #     Used: [Median of Two Sorted Arrays - Binary Search - Leetcode 4](https://www.youtube.com/watch?v=q6IEA26hvXc)
 #     O(log(n+m))
 #     """
-#     A, B = nums1, nums2
-#     if len(A) < len(B):
-#         A, B = B, A
