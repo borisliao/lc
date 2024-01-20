a4e55a35b5875899db2fddeef5f58e0cd761cd9c
borisliao <borisliaos@gmail.com>
2024-01-19 22:51:18 -0500

    Reviewed 11. Container With Most Water

--- Two Pointers/11. Container With Most Water/solution_11.py
+++ Two Pointers/11. Container With Most Water/solution_11.py
@@ -28,3 +28,30 @@ def maxArea(height: list[int]) -> int:
             r -= 1
 
     return largest
+
+
+def review1(height: list[int]) -> int:
+    """
+    Anki 1-19-24
+    Time: 20 min
+    Used: [Container with Most Water - Leetcode 11 - Python](https://www.youtube.com/watch?v=UuiTKBwPgAo)
+    """
+    l = 0
+    r = len(height) - 1
+    water = 0
+    while l < r:
+        # d1 max(water, d2 max(height[l]
+        water = max(water, min(height[l], height[r]) * (r-l))
+        if height[l] > height[r]:
+            r -= 1
+        else:
+            l += 1
+
+    return water
+
+
+# def review2(height: list[int]) -> int:
+#     """
+#     Anki 1-19-24
+#     Time: 20 min
+#     """
