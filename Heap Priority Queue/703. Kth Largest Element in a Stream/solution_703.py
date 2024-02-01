76be9dffdd9267f3ca846c1016663325d540a8fd
borisliao <borisliaos@gmail.com>
2024-01-31 23:13:48 -0500

    Reviewed 703. Kth Largest Element in a Stream

--- Heap Priority Queue/703. Kth Largest Element in a Stream/solution_703.py
+++ Heap Priority Queue/703. Kth Largest Element in a Stream/solution_703.py
@@ -1,10 +1,48 @@
+import heapq
+
+
 # def KthLargest():
 #     class KthLargest:
 
 #         def __init__(self, k: int, nums: list[int]):
-#             pass
+#             self.heap = nums
+#             self.k = k
+#             heapq.heapify(self.heap)
 
 #         def add(self, val: int) -> int:
-#             pass
+#             heapq.heappush(self.heap, val)
+#             return self.heap
+
+#     return KthLargest
+
+def KthLargest():
+    """
+    Anki 1-31-24
+    Used: Debugger 1
+    """
+    class KthLargest:
+
+        def __init__(self, k: int, nums: list[int]):
+            self.k = k
+            self.heap = nums  # d1 []
+            heapq.heapify(self.heap)
+            while len(self.heap) > self.k:
+                heapq.heappop(self.heap)
+
+        def add(self, val: int) -> int:
+            heapq.heappush(self.heap, val)
+            if len(self.heap) > self.k:
+                heapq.heappop(self.heap)
+            return self.heap[0]
+
+    return KthLargest
+
+
+# def review1():
+#     class KthLargest:
+
+#         def __init__(self, k: int, nums: list[int]):
+
+#         def add(self, val: int) -> int:
 
 #     return KthLargest
