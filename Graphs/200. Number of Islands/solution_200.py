116122fb37e76cdd0f15d0bc822dcc412f0915ac
borisliao <borisliaos@gmail.com>
2024-01-31 21:35:17 -0500

    Reviewed 200. Number of Islands

--- Graphs/200. Number of Islands/solution_200.py
+++ Graphs/200. Number of Islands/solution_200.py
@@ -1,2 +1,31 @@
-# def numIslands(grid: list[list[str]]) -> int:
-#     pass
+def numIslands(grid: list[list[str]]) -> int:
+    """
+    1-31-24
+    Time: 14:25
+    Used: Debugger 1
+    """
+    islands = 0
+
+    def setTo0(r, c):
+        if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[r]) or grid[r][c] == '0':
+            return
+        grid[r][c] = '0'
+        setTo0(r-1, c)
+        setTo0(r+1, c)
+        setTo0(r, c-1)
+        setTo0(r, c+1)
+
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == '1':  # d1 == 1 (int)
+                islands += 1
+                setTo0(r, c)
+
+    return islands
+
+# def review1(grid: list[list[str]]) -> int:
+#     """
+#     1-31-24
+#     Time: 14:25
+#     Used: Debugger 1
+#     """
