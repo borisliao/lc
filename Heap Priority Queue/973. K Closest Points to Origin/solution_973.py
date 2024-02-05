808b5cdefb8d80f6ef68d5e097c0ed05121c6d7d
borisliao <borisliaos@gmail.com>
2024-02-05 18:25:24 -0500

    Reviewed 973. K Closest Points to Origin

--- Heap Priority Queue/973. K Closest Points to Origin/solution_973.py
+++ Heap Priority Queue/973. K Closest Points to Origin/solution_973.py
@@ -1,1 +1,21 @@
-# def kClosest(points: list[list[int]], k: int) -> list[list[int]]:
+import heapq
+import math
+
+
+def kClosest(points: list[list[int]], k: int) -> list[list[int]]:
+    """
+    Anki 2-5-24
+    Time: 15 min
+    """
+    distances = []
+
+    for x, y in points:
+        d = abs(math.sqrt(math.pow(x, 2) + math.pow(y, 2)))  # d1 , 2
+        heapq.heappush(distances, (d, (x, y)))  # d3 -d
+
+    result = []
+    for _ in range(k):  # d2 len(k)
+        d, point = heapq.heappop(distances)
+        result.append(list(point))
+
+    return result
