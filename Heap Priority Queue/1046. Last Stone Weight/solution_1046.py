08ef35eff42bece85df0c7d89beea89ecbacbb14
borisliao <borisliaos@gmail.com>
2024-02-05 15:56:09 -0500

    Reviewed 1046. Last Stone Weight

--- Heap Priority Queue/1046. Last Stone Weight/solution_1046.py
+++ Heap Priority Queue/1046. Last Stone Weight/solution_1046.py
@@ -16,3 +16,20 @@ def lastStoneWeight(stones: list[int]) -> int:
         if y-x < 0:
             heapq.heappush(stones, y-x)
     return -stones[0] if stones else 0
+
+
+def review1(stones: list[int]) -> int:
+    """
+    Anki 2-5-24
+    Used: debugger 2
+    Time: 5:34
+    """
+    stones = [-i for i in stones]
+    heapq.heapify(stones)
+
+    while len(stones) >= 2:
+        y = heapq.heappop(stones)
+        x = heapq.heappop(stones)
+        if y-x < 0:  # d2 >
+            heapq.heappush(stones, y-x)
+    return -stones[0] if stones else 0  # d1 -
