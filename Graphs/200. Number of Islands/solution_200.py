ec57daefb8a6248353a8f5077dce8ee46747a267
borisliao <borisliaos@gmail.com>
2024-02-06 22:08:00 -0500

    Reviewed 200. Number of Islands

--- Graphs/200. Number of Islands/solution_200.py
+++ Graphs/200. Number of Islands/solution_200.py
@@ -23,9 +23,35 @@ def numIslands(grid: list[list[str]]) -> int:
 
     return islands
 
-# def review1(grid: list[list[str]]) -> int:
-#     """
-#     1-31-24
-#     Time: 14:25
-#     Used: Debugger 1
-#     """
+
+def review1(grid: list[list[str]]) -> int:
+    """
+    Anki 2-6-24
+    Time: 12 min
+    Used: debugger 1
+    """
+    def markIsland(r, c):
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[r]) or grid[r][c] == '0':
+            return
+        grid[r][c] = '0'
+        markIsland(r-1, c)
+        markIsland(r+1, c)
+        markIsland(r, c-1)
+        markIsland(r, c+1)
+
+    islands = 0
+    for r in range(len(grid)):  # d1
+        for c in range(len(grid[r])):  # d1
+            if grid[r][c] == '1':
+                islands += 1
+                markIsland(r, c)
+
+    return islands
+
+# def review2(grid: list[list[str]]) -> int:
+    # """
+    # Anki 2-6-24
+    # Time: 12 min
+    # Used: debugger 1
+    # do bfs
+    # """
