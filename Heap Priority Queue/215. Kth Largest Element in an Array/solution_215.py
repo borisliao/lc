d1e58c1541bb972a75249f972b6551ac2cdce095
borisliao <borisliaos@gmail.com>
2024-02-07 12:28:51 -0500

    Added 215. Kth Largest Element in an Array

--- Heap Priority Queue/215. Kth Largest Element in an Array/solution_215.py
+++ Heap Priority Queue/215. Kth Largest Element in an Array/solution_215.py
@@ -1,1 +1,14 @@
-# def findKthLargest( nums: list[int], k: int) -> int:
+import heapq
+
+
+def findKthLargest(nums: list[int], k: int) -> int:
+    """
+    Anki 2-7-24
+    Time: 6 min
+    """
+    heapq.heapify(nums)
+
+    while len(nums) > k:
+        heapq.heappop(nums)
+
+    return nums[0]
