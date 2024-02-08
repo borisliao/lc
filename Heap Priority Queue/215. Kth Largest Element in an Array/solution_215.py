492d288101a677075cf8e5277249eb101c2d2b35
borisliao <borisliaos@gmail.com>
2024-02-08 01:20:29 -0500

    Reviewed 215. Kth Largest Element in an Array

--- Heap Priority Queue/215. Kth Largest Element in an Array/solution_215.py
+++ Heap Priority Queue/215. Kth Largest Element in an Array/solution_215.py
@@ -12,3 +12,37 @@ def findKthLargest(nums: list[int], k: int) -> int:
         heapq.heappop(nums)
 
     return nums[0]
+
+
+def review1(nums: list[int], k: int) -> int:
+    """
+    Anki 2-8-24
+    Time: 4 min
+    Used: https://www.youtube.com/watch?v=AzDs7qV1ugA
+    """
+    heap = []
+
+    for n in nums:
+        heapq.heappush(heap, n)
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return heap[0]
+
+
+def review2(nums: list[int], k: int) -> int:
+    """
+    Anki 2-8-24
+    Time: 2 min
+    Used: https://www.youtube.com/watch?v=AzDs7qV1ugA
+    """
+    heap = []
+
+    for n in nums:
+        if len(heap) < k:
+            heapq.heappush(heap, n)
+        elif n > heap[0]:
+            heapq.heappop(heap)
+            heapq.heappush(heap, n)
+
+    return heap[0]
