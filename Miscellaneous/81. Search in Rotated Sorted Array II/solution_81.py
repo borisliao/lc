9efe7f02e85b0508195e7856b88fbb42a8f52414
borisliao <borisliaos@gmail.com>
2024-04-08 14:23:39 -0400

    Reviewed 81. Search in Rotated Sorted Array II

--- Miscellaneous/81. Search in Rotated Sorted Array II/solution_81.py
+++ Miscellaneous/81. Search in Rotated Sorted Array II/solution_81.py
@@ -201,3 +201,31 @@ def review5(nums: list[int], target: int) -> bool:
             l += 1
 
     return False
+
+
+def review6(nums: list[int], target: int) -> bool:
+    """
+    Mochi 4-8-24
+    Time: 30 min
+    """
+    l = 0
+    r = len(nums) - 1
+
+    while l <= r:
+        m = (l+r)//2
+        if nums[m] == target:
+            return True
+        if nums[l] < nums[m]:
+            if nums[l] <= target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        elif nums[l] > nums[m]:
+            if nums[m] < target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+        else:
+            l += 1
+
+    return False
