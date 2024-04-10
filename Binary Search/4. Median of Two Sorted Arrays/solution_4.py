1e8834e4bd7a8fcf0e159525c44a2e844becdf6a
borisliao <borisliaos@gmail.com>
2024-04-09 22:02:20 -0400

    Reviewed 4. Median of Two Sorted Arrays

--- Binary Search/4. Median of Two Sorted Arrays/solution_4.py
+++ Binary Search/4. Median of Two Sorted Arrays/solution_4.py
@@ -338,3 +338,39 @@ def review9(nums1: list[int], nums2: list[int]) -> float:
             r = i - 1
         else:
             l = i + 1
+
+
+def review10(nums1: list[int], nums2: list[int]) -> float:
+    """
+    Mochi 4-9-24
+    Time: 1 hour
+    """
+    A, B = nums1, nums2
+
+    if len(nums1) > len(nums2):
+        A, B = B, A
+
+    total = len(A) + len(B)
+    half = total // 2
+
+    l = 0
+    r = len(A) - 1
+    while True:
+        i = (l+r)//2
+        j = half - 2 - i
+
+        aLeft = A[i] if i >= 0 else float('-inf')
+        aRight = A[i+1] if i < len(A) - 1 else float('inf')
+        bLeft = B[j] if j >= 0 else float('-inf')
+        bRight = B[j+1] if j+1 < len(B) else float('inf')
+
+        if aLeft <= bRight and bLeft <= aRight:
+            if total % 2:  # odd
+                # right because half interger divided by 2 (rounded down)
+                return min(aRight, bRight)
+            else:
+                return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
+        elif aLeft > bRight:
+            r = i - 1
+        else:
+            l = i + 1
