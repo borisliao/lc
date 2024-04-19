868e3d8cf5aeb96587b1d9c9c52e1f37e6002b9c
borisliao <borisliaos@gmail.com>
2024-04-19 15:39:20 -0400

    Reviewed 240. Search a 2D Matrix II

--- Binary Search/240. Search a 2D Matrix II/solution_240.py
+++ Binary Search/240. Search a 2D Matrix II/solution_240.py
@@ -27,3 +27,32 @@ def searchMatrix(matrix: list[list[int]], target: int) -> bool:
             elif target < matrix[row][m]:
                 j = m - 1
     return False
+
+
+def review1(matrix: list[list[int]], target: int) -> bool:
+    """
+    Mochi 4-19-24
+    """
+    l = 0
+    r = len(matrix) - 1
+
+    while l < r:
+        if matrix[r][0] > target:
+            r -= 1
+        elif matrix[l][-1] < target:
+            l += 1
+        else:
+            break
+
+    for row in range(l, r+1):
+        i, j = 0, len(matrix[row]) - 1
+
+        while i <= j:
+            m = (i+j)//2
+            if matrix[row][m] == target:
+                return True
+            if matrix[row][m] < target:
+                i += 1
+            else:
+                j -= 1
+    return False
