24c4efa3af23482bc17000e9f992698875c7dfe9
borisliao <borisliaos@gmail.com>
2024-04-24 23:18:27 -0400

    Reviewed 680. Valid Palindrome II

--- Miscellaneous/680. Valid Palindrome II/solution_680.py
+++ Miscellaneous/680. Valid Palindrome II/solution_680.py
@@ -37,3 +37,18 @@ def neetcode(s: str) -> bool:
         l += 1
         r -= 1
     return True
+
+
+def review1(s: str) -> bool:
+    """
+    Mochi 4-24-24
+    """
+    l = 0
+    r = len(s) - 1
+    while l < r:
+        if s[l] != s[r]:
+            return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
+        l += 1
+        r -= 1
+
+    return True
