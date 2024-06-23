2ccb936a2dcfb25368c61c5960b4de715b162bed
borisliao <borisliaos@gmail.com>
2024-06-23 16:13:38 -0400

    Reviewed 295. Find Median from Data Stream

--- Heap Priority Queue/295. Find Median from Data Stream/solution_295.py
+++ Heap Priority Queue/295. Find Median from Data Stream/solution_295.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 def MedianFinderLinear():
     """
     6-21-24
@@ -66,6 +69,40 @@ def MedianFinderBinary():
 
     return MedianFinder()
 
+
+def neetcode():
+    """
+    6-23-24
+    """
+    class MedianFinder:
+
+        def __init__(self):
+            self.small = []  # maxheap
+            self.large = []  # minheap
+
+        def addNum(self, num: int) -> None:
+            heapq.heappush(self.small, -1*num)
+
+            # make sure every num small is <= every num in large
+            if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
+                val = -1 * heapq.heappop(self.small)
+                heapq.heappush(self.large, val)
+
+            # uneven size
+            if len(self.small) - 1 > len(self.large):
+                val = -1 * heapq.heappop(self.small)
+                heapq.heappush(self.large, val)
+            elif len(self.small) < len(self.large) - 1:
+                val = heapq.heappop(self.large)
+                heapq.heappush(self.small, -1*val)
+
+        def findMedian(self) -> float:
+            if (len(self.small) + len(self.large)) % 2:  # odd
+                return (-1*self.small[0]) if len(self.small) > len(self.large) else self.large[0]
+            else:
+                return (-1*(self.small[0]) + self.large[0])/2
+
+    return MedianFinder()
 # def MedianFinder():
 #     class MedianFinder:
 
--- Heap Priority Queue/295. Find Median from Data Stream/test_solution_295.py
+++ Heap Priority Queue/295. Find Median from Data Stream/test_solution_295.py
@@ -12,3 +12,32 @@ def test_example_1(f):
     assert medianFinder.findMedian() == 1.5  # return  (i.e., (1 + 2) / 2)
     medianFinder.addNum(3)  # arr = [1, 2, 3]
     assert medianFinder.findMedian() == 2.0
+
+
+@pytest.mark.timeout(3)
+@pytest.mark.parametrize("f", [f[1] for f in inspect.getmembers(solution_295, predicate=inspect.isfunction)])
+def test_lc_9(f):
+    """
+    When using 2 heaps, make sure they are actually balanced
+    """
+    medianFinder = f()
+    medianFinder.addNum(1)
+    assert medianFinder.findMedian() == 1.00000
+    medianFinder.addNum(2)
+    assert medianFinder.findMedian() == 1.50000
+    medianFinder.addNum(3)
+    assert medianFinder.findMedian() == 2.00000
+    medianFinder.addNum(4)
+    assert medianFinder.findMedian() == 2.50000
+    medianFinder.addNum(5)
+    assert medianFinder.findMedian() == 3.00000
+    medianFinder.addNum(6)
+    assert medianFinder.findMedian() == 3.50000
+    medianFinder.addNum(7)
+    assert medianFinder.findMedian() == 4.00000
+    medianFinder.addNum(8)
+    assert medianFinder.findMedian() == 4.50000
+    medianFinder.addNum(9)
+    assert medianFinder.findMedian() == 5.00000
+    medianFinder.addNum(10)
+    assert medianFinder.findMedian() == 5.50000
