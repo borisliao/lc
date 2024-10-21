4f08546c109f8654d020f6a8b2d78ddc3657eb36
borisliao <borisliaos@gmail.com>
2024-10-20 22:19:25 -0400

    Reviewed 567. Permutation in String

--- Sliding Window/567. Permutation in String/solution_567.py
+++ Sliding Window/567. Permutation in String/solution_567.py
@@ -412,3 +412,34 @@ def review15(s1: str, s2: str) -> bool:
             window[s2[l]] -= 1
             l += 1
     return False
+
+
+def review16(s1: str, s2: str) -> bool:
+    """
+    Mochi 10-20-24
+    """
+    count = {}
+    for c in s1:
+        count[c] = count.get(c, 0) + 1
+    window = {}
+
+    l = 0
+    for r in range(len(s2)):
+        window[s2[r]] = window.get(s2[r], 0) + 1
+
+        if r+1-l == len(s1):
+            match = True
+            for c in window:
+                if c in count and count[c] == window[c]:
+                    pass
+                else:
+                    match = False
+            if match:
+                return True
+
+            window[s2[l]] -= 1
+            if window[s2[l]] == 0:
+                del window[s2[l]]
+            l += 1
+
+    return False
